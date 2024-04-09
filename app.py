a9346ee670c19baaeaff6f7554a73d008a42f674
Rishabh Mishra <rishabm1301@gmail.com>
2024-04-09 15:44:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def recruitment():
 
 # ========================python main===================================================
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
